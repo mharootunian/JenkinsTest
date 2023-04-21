a387979ae95322cd4eb52fca0a8e394ba50cc153
Meero Harootunian <meero.harootunian@gmail.com>
2023-04-21 10:37:10 -0700

    first

---
+++ SalesOrderEntryExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PX.Data;
+using PX.Objects.SO;
+
+namespace JenkinsTest
+{
+	public class SalesOrderEntryExtension : PXGraphExtension<SOOrderEntry>
+	{
+        public override void Initialize() {
+            base.Initialize();
+        }
+
+        protected virtual void SOOrder_RowSelected(PXCache sender, PXRowSelectedEventArgs e) {
+            SOOrder row = e.Row as SOOrder;
+            if (row == null) return;
+
+            if (row.OrderType == "SO") {
+                PXUIFieldAttribute.SetEnabled<SOOrder.orderType>(sender, row, false);
+                PXTrace.WriteError("<!>!!!!!!!<!>SO SELECTED <!>!!!!!!!<!>");
+            }
+        }   
+	}
+}
